f14742183bf8b8d56b9608729e443d7190ae8602
GabrielSa <gabrielsa95.2@gmail.com>
2024-02-13 16:18:33 -0300

    InvoiceEmitter: Illustration when form submited

--- src/components/closeout/InvoiceEmitter.tsx
+++ src/components/closeout/InvoiceEmitter.tsx
@@ -1,11 +1,20 @@
 
+
 import { Button, Dialog } from "@radix-ui/themes";
-import { CheckCircle2Icon } from "lucide-react";
-import { useState } from "react";
+import { useForm} from "react-hook-form";
 import { toast } from "sonner";
+import {z} from "zod";
+
+import WomanSentCheck from '../../svg/woman-sent-check.svg'
 
 
+const invoiceEmitterForm = z.object({
+    owner_document: z.string(),
+    description: z.string(),
+    value: z.number(),
+})
 
+type invoiceEmitter = z.infer<typeof invoiceEmitterForm>
 
 export interface InvoiceEmitterProps {
     owner_name: string;
@@ -15,18 +24,23 @@ export interface InvoiceEmitterProps {
 }
 //should be a form
 export function InvoiceEmitter(props: InvoiceEmitterProps){
-    
-    const [emittButtonDisabled, setEmittButtonDisabled] = useState(false); 
-    const [emittButtonLabel, setEmittButtonLabel] = useState('Emitir');
-  
+    const {register, handleSubmit, formState: {isSubmitting, isSubmitted}} = useForm<invoiceEmitter>();
 
+    
 
     async function handleEmitterButtonClick(){
-        setEmittButtonDisabled(true)
-        setEmittButtonLabel('Aguarde')        
-        toast.success("Nota fiscal enviada para emissão.")
+        
+        console.log(isSubmitting, isSubmitted)
+
+        
     }
     
+
+
+
+    if(isSubmitted){
+        return <EmittionSent />
+    }
     
     
     function handleTemplate(month: string, year: number | string){
@@ -43,12 +57,52 @@ export function InvoiceEmitter(props: InvoiceEmitterProps){
         )  
     }
     return(
-        <div>
+        <form onSubmit={handleSubmit(handleEmitterButtonClick)}>
             <div className=" h-3/4 pl-3 mt-5">
-                <p className="font-semibold text-zinc-600">Proprietário: <span className="font-normal">{props.owner_name}</span></p>
-                <p className="font-semibold text-zinc-600">CPF/CNPJ: <span>{props.owner_document}</span></p>
-                <p className="font-semibold text-zinc-600">Valor da Nota: <span>{props.management_fee}</span></p>
-                <p className="font-semibold text-zinc-600">ISS Retido: <span>Não</span></p>
+                
+                <div>
+                    <label className="font-semibold text-zinc-600">
+                        Proprietário: <span className="text-zinc-500 font-normal">{props.owner_name}</span>
+                    </label>
+                    
+                    <input 
+                        id="owner_name" 
+                        defaultValue={props.owner_name}
+                        className="hidden"
+                    />
+                </div>
+                
+                
+                <div>
+                    <label className="font-semibold text-zinc-600">
+                        CPF/CNPJ: <span className="text-zinc-500 font-normal">{props.owner_document}</span>
+                    </label>
+                    <input 
+                        id="owner_document" 
+                        {...register('owner_document')}
+                        defaultValue={props.owner_document}     
+                        className="hidden"
+                    />
+                </div>
+                    
+    
+                <div>
+                    <label className="font-semibold text-zinc-600">
+                        Valor da Nota: <span className="text-zinc-500 font-normal">{props.management_fee}</span>
+                    </label>
+                    
+                    <input 
+                        id="management_fee" 
+                        {...register('value')}
+                        className="hidden"
+                    />                
+                </div>
+                
+                <label className="font-semibold text-zinc-600">
+                    ISS Retido: <span className="text-zinc-500 font-normal">Não</span>
+                </label>
+                    
+                
 
                 <h1 className=" mt-2  text-zinc-600">Descrição:</h1>
                 
@@ -59,15 +113,16 @@ export function InvoiceEmitter(props: InvoiceEmitterProps){
             <footer className=" flex gap-2 justify-center  m-5">                
                 <Dialog.Close>
                     <Button 
+                        type="submit"
                         className="w-1/2"
-                        onClick={handleEmitterButtonClick}
-                        disabled={emittButtonDisabled}
+                        onClick={handleSubmit(handleEmitterButtonClick)}
+                        disabled={isSubmitting}
                     >
-                        {emittButtonLabel}
+                        Emitir
                     </Button>
                 </Dialog.Close>
             </footer>
-        </div>
+        </form>
            )
 }
 
@@ -76,7 +131,7 @@ export function EmittionSent(){
         <div className="flex flex-col w-full h-full items-center">
             <h1 className="text-xl font-bold text-zinc-500">Nota fiscal enviada para emissão</h1>
             <p className="text-zinc-400"> em breve o status será atualizado</p>
-            <CheckCircle2Icon className="w-20 h-20 text-green-500 mt-5" />
+            <img src={WomanSentCheck} width={100} height={100} className="mt-5" />
 
         </div>
     )
